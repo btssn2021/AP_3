a814f7af49d9cc1480d9ec532821a732f678f17d
eric <eric.thomas@formation-industries-fc.fr>
2021-10-07 11:22:17 +0200

    ex3

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
     }
 
     /************************************Exercice n°2************************************/
-    int somme=0;
+    /*int somme=0;
     for(int i=1;i<=10;i++)
     {
         printf("%d ",i);
@@ -17,6 +17,16 @@ int main() {
     }
     printf("\nla somme est:%d",somme);
     /***********************************Exercice n°3**************************************/
+    int sommed=0,n=0;
+    printf("Combien de termes?\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d ",i);
+        sommed+=i; //equivalent à somme=somme+i
+
+    }
+    printf("\nla somme est:%d",sommed);
 
     return 0;
 }
